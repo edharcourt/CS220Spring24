c3afab919fab8031332b74d253e874b6713c52b4
Ed Harcourt <edharcourt@stlawu.edu>
2024-03-10 15:27:22 -0400

    integer f2c

---
+++ assembly/f2c_int/f2c.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+int64_t f2c(int64_t t);
---
+++ assembly/f2c_int/main.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "f2c.h"
+
+int main() {
+
+    printf("%lld\n", f2c(212));
+    return 0;  // return code back to OS
+}
